af9b789e1cbbd31f1073fbf4eb45b616fb2558b7
fares <fares.steen@gmail.com>
2022-04-06 09:57:47 +0200

    add client

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Grpc.Net.Client;
+using GrpcServer;
+
+var input = new HelloRequest {Name = "Fares"};
+
+var channel = GrpcChannel.ForAddress("http://localhost:5054");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+Console.ReadLine();
